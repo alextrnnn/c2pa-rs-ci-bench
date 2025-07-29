64a8ded31906506288e0f08a79b51076a254be58
Alex Tran <alextrnnn@gmail.com>
2025-07-28 18:07:58 -0700

    test: Add more delay

--- sdk/src/builder.rs
+++ sdk/src/builder.rs
@@ -1049,7 +1049,7 @@ impl Builder {
 
         use std::{thread, time};
 
-        let ten_millis = time::Duration::from_millis(10);
+        let ten_millis = time::Duration::from_millis(1000);
 
         thread::sleep(ten_millis);
 
